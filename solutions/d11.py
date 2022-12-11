264bfac2ba54b3eea5160a61ccd3f9183eca222e
Sean Malloy <techfan321@gmail.com>
2022-12-11 13:48:05 -0500

    Day 11 optimizations

--- solutions/d11.py
+++ solutions/d11.py
@@ -2,7 +2,6 @@ from io import TextIOWrapper
 
 import heapq
 from dataclasses import dataclass
-from collections import deque
 from enum import Enum
 from copy import deepcopy
 from functools import reduce
@@ -11,47 +10,48 @@ class Op(Enum):
     ADD = 0
     MUL = 1
 
-@dataclass
 class Expr:
-    lhs: str
     op: Op
-    rhs: str
+    rhs: int
+
+    def __init__(self, op: Op, rhs: str):
+        self.op = op
+        self.rhs = -1 if rhs == 'old' else int(rhs)
 
     def eval(self, old: int) -> int:
-        l = old if self.lhs == 'old' else int(self.lhs)
-        r = old if self.rhs == 'old' else int(self.rhs)
-        if self.op == Op.ADD:
-            return l + r
-        return l * r
+        r = old if self.rhs < 0 else self.rhs
+        return old + r if self.op == Op.ADD else old * r
 
 @dataclass
 class Monkey:
-    items: deque[int]
+    items: list[int]
     op: Expr
     test: int
     true: int
     false: int
-    num_inspected = 0
+    num_inspected: int = 0
 
 def do_div_round(monkeys: list[Monkey]):
     for m in monkeys:
         m.num_inspected += len(m.items)
-        for _ in range(len(m.items)):
-            worry = m.op.eval(m.items.popleft()) // 3
+        for i in m.items:
+            worry = m.op.eval(i) // 3
             if worry % m.test == 0:
                 monkeys[m.true].items.append(worry)
             else:
                 monkeys[m.false].items.append(worry)
+        m.items.clear()
 
 def do_mod_round(monkeys: list[Monkey], modulo: int):
     for m in monkeys:
         m.num_inspected += len(m.items)
-        for _ in range(len(m.items)):
-            worry = m.op.eval(m.items.popleft()) % modulo
+        for i in m.items:
+            worry = m.op.eval(i) % modulo
             if worry % m.test == 0:
                 monkeys[m.true].items.append(worry)
             else:
                 monkeys[m.false].items.append(worry)
+        m.items.clear()
 
 def main(file: TextIOWrapper):
     notes = file.read().split('\n\n')
@@ -59,11 +59,11 @@ def main(file: TextIOWrapper):
     for m in notes:
         lines = m.split('\n')
 
-        items = deque(map(int, lines[1].split(': ')[1].split(',')))
+        items = list(map(int, lines[1].split(': ')[1].split(',')))
         
-        lhs, str_op, rhs = lines[2].split('= ')[1].split()
+        _, str_op, rhs = lines[2].split('= ')[1].split()
         op = Op.ADD if str_op == '+' else Op.MUL
-        expr = Expr(lhs, op, rhs)
+        expr = Expr(op, rhs)
 
         test = int(lines[3].split()[-1])
         true = int(lines[4].split()[-1])
