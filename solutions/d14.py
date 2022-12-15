7e6200a982eaaa0402ec63cea738b22fe4829493
Sean Malloy <techfan321@gmail.com>
2022-12-14 20:48:16 -0500

    Day 14

--- solutions/d14.py
+++ solutions/d14.py
@@ -25,19 +25,18 @@ def walk(start: Point, end: Point) -> set[Point]:
 
 def print_cave(min_x, min_y, max_x, max_y, walls, sand):
     s = ''
-    for y in range(min_y-10, max_y+10):
-        for x in range(min_x-10, max_x+10):
-            curr = Point(x, y)
-            if curr in walls:
+    for y in range(-10, max_y+2):
+        for x in range(min_x-200, max_x+200):
+            src = Point(x, y)
+            if is_wall(src, walls, max_y, True):
                 s += '#'
-            elif curr in sand:
+            elif src in sand:
                 s += 'o'
             else:
                 s += '.'
         s += '\n'
     system('clear')
     print(s)
-    sleep(0.1)
 
 
 def is_wall(pt: Point, walls: set[Point], max_y: int, part2: bool) -> bool:
@@ -47,31 +46,30 @@ def is_wall(pt: Point, walls: set[Point], max_y: int, part2: bool) -> bool:
 def drop(src: Point, walls: set[Point], sand: set[Point], max_y: int, part2: bool):
     x = src.x
     y = src.y
-    curr = Point(x, y)
-    while not is_wall(curr, walls, max_y, part2) and curr not in sand and y <= max_y:
+    while not is_wall(src, walls, max_y, part2) and src not in sand and y <= max_y:
         y += 1
-        curr = Point(x, y)
+        src = Point(x, y)
     if y > max_y:
         return False
     
-    if is_wall(curr, walls, max_y, part2):
+    if is_wall(src, walls, max_y, part2):
         y -= 1
-        sand.add(Point(curr.x, y))
+        sand.add(Point(src.x, y))
         return True
-    while is_wall(curr, walls, max_y, part2) or curr in sand and curr.y <= max_y:
-        left = Point(curr.x - 1, curr.y)
-        right = Point(curr.x + 1, curr.y)
+    while is_wall(src, walls, max_y, part2) or src in sand and src.y <= max_y:
+        left = Point(src.x - 1, src.y)
+        right = Point(src.x + 1, src.y)
         if is_wall(left, walls, max_y, part2) or left in sand:
             if is_wall(right, walls, max_y, part2) or right in sand:
-                sand.add(Point(curr.x, curr.y - 1))
+                sand.add(Point(src.x, src.y - 1))
                 return True
             else:
-                curr = Point(right.x, right.y + 1)
+                src = Point(right.x, right.y + 1)
         else:
-            curr = Point(left.x, left.y + 1)
+            src = Point(left.x, left.y + 1)
             
-    if not is_wall(Point(curr.x, curr.y + 1), walls, max_y, part2) or Point(curr.x, curr.y + 1) not in sand:
-        return drop(curr, walls, sand, max_y, part2)
+    if not is_wall(Point(src.x, src.y + 1), walls, max_y, part2) or Point(src.x, src.y + 1) not in sand:
+        return drop(src, walls, sand, max_y, part2)
     return False
 
 
@@ -92,7 +90,7 @@ def main(file: TextIOWrapper):
             max_x = max(max_x, start.x, end.x)
             min_y = min(min_y, start.y, end.y)
             max_y = max(max_y, start.y, end.y)
-
+        
     src = Point(500, 0)
     sand = set()
     dropping = drop(src, walls, sand, max_y, False)
